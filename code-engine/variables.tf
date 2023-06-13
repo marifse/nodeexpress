290922cd27127193a9dc4f42b1179ba4b921e125
Ubuntu <ubuntu@ip-172-31-24-248.us-west-2.compute.internal>
2023-06-13 20:29:26 +0000

    updated

---
+++ code-engine/variables.tf
@@ -0,0 +1,37 @@
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API key"
+  type        = string
+  default = "xxxxxxxxxxxxxxxxxxx"
+}
+
+
+variable "code_engine_build_output_secret" {
+  description = "The secret that is required to access the image registry. Make sure that the secret is granted with push permissions towards the specified container registry namespace."
+  type        = string
+  default     = "ce-auto-icr-private-us-south"
+}
+
+variable "namespace_name" {
+  type = string
+  default = "nodeexpress_namespace_1"
+}
+
+variable "application_name" {
+  type = string
+  default = "nodeexpree-app"
+}
+
+variable "imageURLRegistry" {
+  type = string
+  default = "private.us.icr.io"
+}
+
+variable "authRegitry" {
+  type = string
+  default = "us.icr.io"
+}
+
+variable "projectName" {
+  type = string
+  default = "nodeexpress-ce"
+}
